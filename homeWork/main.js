f17872b69dfc415f61342e0c46978e9835b00f77
AlehZahorski <git3371m@gmail.com>
2020-04-11 14:29:48 +0200

    add clear button

--- homeWork/main.js
+++ homeWork/main.js
@@ -5,16 +5,17 @@ let numElement = 1;
 
 const  init = () =>{
 const button = document.createElement('button');
+const buttonRemove = document.createElement('button');
 const ul = document.createElement('ul');
 document.body.appendChild(button);
+document.body.appendChild(buttonRemove);
 document.body.appendChild(ul);
 button.textContent="Dodaj kolejne 10 elementow";
-button.addEventListener('click', createLiElements)
+buttonRemove.textContent="Oczysc cala liste";
+buttonRemove.style.marginLeft="10px";
+button.addEventListener('click', createLiElements);
+buttonRemove.addEventListener('click', clearUlList);
 }
-
-
-
-
 
 
 const  createLiElements = () =>{
@@ -22,13 +23,18 @@ console.log('dziala no a dlaczego ma nie dzialac');
 const elementUl = document.querySelector('ul');
 for(let i = 0;i<10;i++){
 let newLi = document.createElement('li');
-newLi.textContent=`element numer ${numElement}`;
+newLi.textContent=`element numer ${numElement++}`;
 elementUl.appendChild(newLi);
-newLi.style.fontSize=`${fontSize}px`;
-fontSize++;
-numElement++;
+newLi.style.fontSize=`${fontSize++}px`;
 }
 }
+
+
+const clearUlList = () => {
+document.querySelector('ul').innerHTML ="";
+numElement = 1;
+fontSize = 10;
+} 
 
 
 
